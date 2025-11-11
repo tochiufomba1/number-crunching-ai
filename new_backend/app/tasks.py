678dd9effe94fbaf1ea95152a3fb3b5a92a8d045
Tochi Ufomba <[tochiufomba@gmail.com]>
2025-11-11 09:48:08 -0600

    Change database session access pattern in background tasks

--- new_backend/app/tasks.py
+++ new_backend/app/tasks.py
@@ -12,7 +12,7 @@ import app.models.database_models as db_models
 from typing import Annotated
 from fastapi import UploadFile, Depends, HTTPException
 from mypy_boto3_s3.client import S3Client
-from app.dependencies import get_session, get_s3_client, get_redis_connection
+from app.dependencies import Session, get_s3_client, get_redis_connection
 from botocore.exceptions import ClientError
 
 def create_coa(
@@ -23,57 +23,57 @@ def create_coa(
     """
 
     """
-    session = get_session()
     s3_client = boto3.Session().client("s3")
 
-    try:
-        s3_object = s3_client.get_object(Bucket=os.getenv("BUCKET_NAME"), Key=s3_object_key)
-        file_stream = s3_object["Body"]
-        lf = pl.scan_csv(file_stream, with_column_names=lambda cols: [col.lower() for col in cols])
-    except Exception as e:
-        print(e)
-    else:
-        # check for necessary columns
-        lf_columns = set(lf.collect_schema().names())
-        missing_columns = {"account"} - lf_columns
-        if missing_columns:
-            #app.helpers.emit_job_status(user_id, "tables", f"Failed,Missing required columns: {', '.join(missing_columns)}")
-            return
+    with Session() as session:
+        try:
+            s3_object = s3_client.get_object(Bucket=os.getenv("BUCKET_NAME"), Key=s3_object_key)
+            file_stream = s3_object["Body"]
+            lf = pl.scan_csv(file_stream, with_column_names=lambda cols: [col.lower() for col in cols])
+        except Exception as e:
+            print(e)
+        else:
+            # check for necessary columns
+            lf_columns = set(lf.collect_schema().names())
+            missing_columns = {"account"} - lf_columns
+            if missing_columns:
+                #app.helpers.emit_job_status(user_id, "tables", f"Failed,Missing required columns: {', '.join(missing_columns)}")
+                return
 
-        data = (
-            lf
-            .select(pl.col("account"))
-            .collect()
-        )
-
-        if data.is_empty():
-            #app.helpers.emit_job_status(user_id, "tables", f"Failed,Empty file")
-            return
-        
-        # create new coa group
-        new_coa_group = db_models.COAIDtoGroup(group_name=coa_group_name)
-        session.add(new_coa_group)
-        session.flush()
-
-        coa_group_id = new_coa_group.group_id
-        session.add(
-            db_models.UserCOAAccess(
-                user_id=user_id, 
-                group_id=coa_group_id, 
-                access_level="administrator"
+            data = (
+                lf
+                .select(pl.col("account"))
+                .collect()
             )
-        )
 
-        coa_items = [
-            db_models.COA(group_id=coa_group_id, account=account) 
-            for account in data.get_column("account").unique().str.replace_all(r'_', " ").str.to_titlecase().to_list()
-        ]
-        session.add_all(coa_items)
-        
-        session.commit()
-        # app.helpers.emit_job_status(user_id, "new_coa_group", "Success")
+            if data.is_empty():
+                #app.helpers.emit_job_status(user_id, "tables", f"Failed,Empty file")
+                return
+            
+            # create new coa group
+            new_coa_group = db_models.COAIDtoGroup(group_name=coa_group_name)
+            session.add(new_coa_group)
+            session.flush()
 
-        return
+            coa_group_id = new_coa_group.group_id
+            session.add(
+                db_models.UserCOAAccess(
+                    user_id=user_id, 
+                    group_id=coa_group_id, 
+                    access_level="administrator"
+                )
+            )
+
+            coa_items = [
+                db_models.COA(group_id=coa_group_id, account=account) 
+                for account in data.get_column("account").unique().str.replace_all(r'_', " ").str.to_titlecase().to_list()
+            ]
+            session.add_all(coa_items)
+            
+            session.commit()
+            # app.helpers.emit_job_status(user_id, "new_coa_group", "Success")
+
+            return
 
 def create_template(
     template_info: app_models.TemplateInfo,
@@ -88,104 +88,104 @@ def create_template(
     4. Train FastText model on transactions.
     5. Upload trained model to S3.
     """
-    session = get_session()
     s3_client = boto3.Session().client("s3")
 
-    try:
-        transactions_file_path = app.helpers.read_s3_object_to_tempfile(object_key)
-        lf = pl.scan_csv(transactions_file_path, with_column_names=lambda cols: [col.lower() for col in cols])
-    except Exception as e:
-        app.helpers.emit_job_status(user_id, "tables", "Failed,Server error")
-        raise
-    finally:
+    with Session() as session:
         try:
-            os.remove(transactions_file_path)
-            s3_client.delete_object(Bucket=os.getenv('BUCKET_NAME'), Key=object_key)
-        except FileNotFoundError:
-            pass
-        except Exception:
-            pass
-    
-    # check for required columns
-    lf = lf.rename({"description":"memo"}, False)
+            transactions_file_path = app.helpers.read_s3_object_to_tempfile(object_key)
+            lf = pl.scan_csv(transactions_file_path, with_column_names=lambda cols: [col.lower() for col in cols])
+        except Exception as e:
+            app.helpers.emit_job_status(user_id, "tables", "Failed,Server error")
+            raise
+        finally:
+            try:
+                os.remove(transactions_file_path)
+                s3_client.delete_object(Bucket=os.getenv('BUCKET_NAME'), Key=object_key)
+            except FileNotFoundError:
+                pass
+            except Exception:
+                pass
+        
+        # check for required columns
+        lf = lf.rename({"description":"memo"}, False)
 
-    lf_columns = set(lf.collect_schema().names())
-    missing_columns = {"description", "account" "amount"} - lf_columns
-    if missing_columns:
-        app.helpers.emit_job_status(user_id, "tables", f"Failed,Missing required columns: {', '.join(missing_columns)}")
-        return
+        lf_columns = set(lf.collect_schema().names())
+        missing_columns = {"description", "account" "amount"} - lf_columns
+        if missing_columns:
+            app.helpers.emit_job_status(user_id, "tables", f"Failed,Missing required columns: {', '.join(missing_columns)}")
+            return
 
-    data = (
-        lf
-        .select(["description", "account", "amount"]) # do modifications on 'account' rep
-        .collect()
-    )
-
-    if data.is_empty():
-        app.helpers.emit_job_status(user_id, "tables", f"Failed,Empty file")
-        return
-
-    # Step 2: Create new COA group, if need be 
-    coa_group_id = template.coa_group_id
-    if coa_group_id == -1:
-        # create new coa group
-        new_coa_group = db_models.COAIDtoGroup(group_name=f"{template_info.title}_COA")
-        session.add(new_coa_group)
-        session.flush()
-
-        coa_group_id = new_coa_group.group_id
-        session.add(
-            db_models.UserCOAAccess(
-                user_id=user_id, 
-                group_id=coa_group_id, 
-                access_level="administrator"
-            )
+        data = (
+            lf
+            .select(["description", "account", "amount"]) # do modifications on 'account' rep
+            .collect()
         )
 
-        coa_items = [
-            db_models.COA(group_id=coa_group_id, account=account) 
-            for account in data.get_column("account").unique().str.replace_all(r'_', " ").str.to_titlecase().to_list()
+        if data.is_empty():
+            app.helpers.emit_job_status(user_id, "tables", f"Failed,Empty file")
+            return
+
+        # Step 2: Create new COA group, if need be 
+        coa_group_id = template.coa_group_id
+        if coa_group_id == -1:
+            # create new coa group
+            new_coa_group = db_models.COAIDtoGroup(group_name=f"{template_info.title}_COA")
+            session.add(new_coa_group)
+            session.flush()
+
+            coa_group_id = new_coa_group.group_id
+            session.add(
+                db_models.UserCOAAccess(
+                    user_id=user_id, 
+                    group_id=coa_group_id, 
+                    access_level="administrator"
+                )
+            )
+
+            coa_items = [
+                db_models.COA(group_id=coa_group_id, account=account) 
+                for account in data.get_column("account").unique().str.replace_all(r'_', " ").str.to_titlecase().to_list()
+            ]
+            session.add_all(coa_items)
+
+        # Step 3: Create Template
+        model_name = secrets.token_hex(16)
+        new_template = db_models.Template(title=template_info.title, model_name=model_name, coa_group_id=coa_group)
+        session.add(new_template)
+        session.flush()
+
+        session.add(db_models.UserTemplateAccess(template_id=new_template.id, user_id=user_id, access_level="administrator"))
+
+        # Step 4: Add transactions to database
+        transactions = [db_models.Transaction(description=row['description'], account=row["account"], amount=row['amount'], template_id=new_template.id) for _, row in data.iterrows()]
+        session.add_all(transactions)
+
+        # Step 5: Clean descriptions + Create training file contents
+        lines = [
+            f"__label__{t.account} {t.description}"
+            for t in transactions
         ]
-        session.add_all(coa_items)
-
-    # Step 3: Create Template
-    model_name = secrets.token_hex(16)
-    new_template = db_models.Template(title=template_info.title, model_name=model_name, coa_group_id=coa_group)
-    session.add(new_template)
-    session.flush()
-
-    session.add(db_models.UserTemplateAccess(template_id=new_template.id, user_id=user_id, access_level="administrator"))
-
-    # Step 4: Add transactions to database
-    transactions = [db_models.Transaction(description=row['description'], account=row["account"], amount=row['amount'], template_id=new_template.id) for _, row in data.iterrows()]
-    session.add_all(transactions)
-
-    # Step 5: Clean descriptions + Create training file contents
-    lines = [
-        f"__label__{t.account} {t.description}"
-        for t in transactions
-    ]
-    
-    # Train and upload model
-    with tempfile.NamedTemporaryFile(mode="w", suffix=".txt") as train_fp, \
-    tempfile.NamedTemporaryFile(mode="w", suffix=".bin") as model_fp:
-        try:
-            # Step 6: Train fasttext model
-            train_fp.write("\n".join(lines))
-            model = fasttext.train_supervised(input=train_fp.name, lr=0.5, epoch=40)
-
-            # Step 7: Upload model to s3
-            model.save_model(model_fp.name)
-            response = s3_client.upload_file(model_fp.name, os.getenv("BUCKET_NAME"), f"{model_name}.bin")
-        except ClientError as e:
-            app.helpers.emit_job_status(user_id, "tables", f"Failed,Couldn't upload your template")
-            return
-        except Exception as e:
-            app.helpers.emit_job_status(user_id, "tables", f"Failed,Server error")
-            return
         
-    session.commit()
-    return
+        # Train and upload model
+        with tempfile.NamedTemporaryFile(mode="w", suffix=".txt") as train_fp, \
+        tempfile.NamedTemporaryFile(mode="w", suffix=".bin") as model_fp:
+            try:
+                # Step 6: Train fasttext model
+                train_fp.write("\n".join(lines))
+                model = fasttext.train_supervised(input=train_fp.name, lr=0.5, epoch=40)
+
+                # Step 7: Upload model to s3
+                model.save_model(model_fp.name)
+                response = s3_client.upload_file(model_fp.name, os.getenv("BUCKET_NAME"), f"{model_name}.bin")
+            except ClientError as e:
+                app.helpers.emit_job_status(user_id, "tables", f"Failed,Couldn't upload your template")
+                return
+            except Exception as e:
+                app.helpers.emit_job_status(user_id, "tables", f"Failed,Server error")
+                return
+            
+        session.commit()
+        return
 
 def process_transactions_task(
     user_id: int,
